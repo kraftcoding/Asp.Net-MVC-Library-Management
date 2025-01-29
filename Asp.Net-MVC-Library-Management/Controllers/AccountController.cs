becb13fdaabf20de99a8a2684c28aec1b09180c7
ielgalzaa <i.elg.alzaa@gmail.com>
2025-01-29 07:32:52 -0800

    Add project files.

---
+++ Asp.Net-MVC-Library-Management/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using AspNetMVCLibraryManagement.Model;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Asp.Net_MVC_Library_Management.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User model)
+        {
+            if (!string.IsNullOrEmpty(model.Email) && !string.IsNullOrEmpty(model.Password))
+            {
+                if (model.Email.ToString() == "admin" && model.Password.ToLower() == "admin")
+                {
+                    //FormsAuthentication.SetAuthCookie(model.Email, true);
+                    //Response.Redirect("/Admin/Index");
+                }
+            }
+            return View();
+        }
+
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            FormsAuthentication.RedirectToLoginPage();
+
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
---
+++ Asp.Net-MVC-Library-Management/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace Asp.Net_MVC_Library_Management.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Response.Redirect("/login");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
